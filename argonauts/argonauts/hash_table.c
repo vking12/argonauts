5aabdbfb84bc15ca59c5ebafd5eca99639810b8f
vking12 <vijayrajmahida@gmail.com>
2018-08-09 08:48:44 -0500

    began hash table implementation

---
+++ argonauts/argonauts/hash_table.c
@@ -0,0 +1,16 @@
+//
+//  hash_table.c
+//  argonauts
+//
+//  Created by Vijayraj Mahida on 8/9/18.
+//  Copyright © 2018 Vijayraj Mahida. All rights reserved.
+//
+
+#include "hash_table.h"
+
+
+
+int init_hash_table(int arc, char **argv){
+    
+    return 0;
+}
---
+++ argonauts/argonauts/hash_table.h
@@ -0,0 +1,14 @@
+//
+//  hash_table.h
+//  argonauts
+//
+//  Created by Vijayraj Mahida on 8/9/18.
+//  Copyright © 2018 Vijayraj Mahida. All rights reserved.
+//
+
+#ifndef hash_table_h
+#define hash_table_h
+
+#include <stdio.h>
+
+#endif /* hash_table_h */
